af6cc3a5f1252347a88106867bc0b5464b5510e3
Prince Pandey <princepandey6150@gmail.com>
2024-09-08 22:36:52 +0530

    question43.java added

---
+++ Preparation-Street-1-solutions/question43.java
@@ -0,0 +1,13 @@
+public class question43 {
+    public static void main(String args[])
+    {
+        int n = 12;
+        for(int i = 1; i <= n; i++)
+        {
+            if( n % i == 0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
