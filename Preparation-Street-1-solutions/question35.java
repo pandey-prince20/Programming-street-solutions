449fe1be0ecfbbf9fd8f7d9f86657224c01dea0e
Prince Pandey <princepandey6150@gmail.com>
2024-09-08 22:19:08 +0530

    question35.java added

---
+++ Preparation-Street-1-solutions/question35.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class question35 {
+    public static boolean isPrime(int n)
+    {
+        if( n == 1)
+        {
+            return false;
+        }
+        if(n == 2)
+        {
+            return true;
+        }
+        for(int i = 2; i < n; i++)
+        {
+            if( n % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+        int sum = 0;
+        for(int i = start; i <= end; i++)
+        {
+            if(isPrime(i))
+            {
+               sum = sum + i;
+            }
+        }
+        System.out.println("sum of prime no: "+ sum);
+    }
+}
