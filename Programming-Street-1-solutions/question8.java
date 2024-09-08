2c76b810d67784e9574df7b00fe3f453206879dc
Prince Pandey <princepandey6150@gmail.com>
2024-09-08 13:07:27 +0530

    question8.java added

---
+++ Programming-Street-1-solutions/question8.java
@@ -0,0 +1,13 @@
+public class question8 {
+    public static void main(String args[])
+    {
+        int fac = 1;
+        int n = 5;
+        while( n  > 0)
+        {
+            fac = fac * n;
+            n--;
+        }
+        System.out.println(fac);
+    }
+}
