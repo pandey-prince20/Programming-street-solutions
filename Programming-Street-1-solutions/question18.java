4f77afc24c994c0f225e822a1512ddc28aa8d5a0
Prince Pandey <princepandey6150@gmail.com>
2024-09-08 13:30:42 +0530

    question18.java added

---
+++ Programming-Street-1-solutions/question18.java
@@ -0,0 +1,10 @@
+public class question18 {
+    public static void main(String args[])
+    {
+        int n = 5;
+        for(int i = 1; i <= 10; i++)
+        {
+            System.out.println(n + " * " + i +" = " + n * i);
+        }
+    }
+}
