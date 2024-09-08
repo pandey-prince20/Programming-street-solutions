efc54b564a6c9d4f688e36395591a39759f1a34b
Prince Pandey <princepandey6150@gmail.com>
2024-09-08 13:24:52 +0530

    question16.java added

---
+++ Programming-Street-1-solutions/question16.java
@@ -0,0 +1,12 @@
+public class question16 {
+    public static void main(String args[])
+    {
+        int arr[] = { 2,12,55,52,242,1,64,23,11};
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++)
+        {
+            sum = sum + arr[i];
+        }
+        System.out.println(sum);
+    }
+}
