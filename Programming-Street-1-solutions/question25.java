142251195af856c5fc976e20ea3a1b2edb2d8b39
Prince Pandey <princepandey6150@gmail.com>
2024-09-08 15:42:56 +0530

    question25.java

---
+++ Programming-Street-1-solutions/question25.java
@@ -0,0 +1,13 @@
+public class question25 {
+    public static void main(String args[])
+    {
+        int n = 12345;
+        int count = 0;
+        while( n > 0)
+        {
+            count++;
+            n = n / 10;
+        }
+        System.out.println(count);
+    }
+}
